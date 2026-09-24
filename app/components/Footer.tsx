c376dcbb9e0ec429f2b02921df441f66916822c1
Aspin Chakma <aspinchakma2019@gmail.com>
2026-09-24 14:54:53 +0600

    finished footer section

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import FooterLogo from "../assets/footerlogo.png";
+
 const Footer = () => {
   return (
-    <div>
-      <h2>This is Footer</h2>
-    </div>
+    <footer className="py-6 border-t border-t-[#1c1f26]">
+      <div className="w-[95%] md:w-[85%] lg:w-[85%] mx-auto flex items-center flex-col lg:flex-row md:flex-row lg:justify-between md:justify-between">
+        <div className="flex gap-2 items-center">
+          <Image src={FooterLogo} className="w-9" alt="fit log footer logo" />
+          <h2 className="font-bold font-oswald text-xl">FITLOG</h2>
+        </div>
+        <p className="text-[#6B7280]">
+          © {new Date().getFullYear()} FitLog — Workout Library. Train hard, log
+          honest.
+        </p>
+      </div>
+    </footer>
   );
 };
 

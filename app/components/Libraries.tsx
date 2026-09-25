1d22acfe6c9eb4a88db42604735f6a386e563346
Aspin Chakma <aspinchakma2019@gmail.com>
2026-09-25 14:16:42 +0600

    finished libraries section

--- app/components/Libraries.tsx
+++ app/components/Libraries.tsx
@@ -12,8 +12,11 @@ const Libraries = () => {
 
   console.log(context?.libraries);
   return (
-    <div>
-      <h3>This is Libraries Section</h3>
+    <div className="mt-12">
+      <h3 className="text-4xl font-bold font-oswald">THE LIBRARY</h3>
+      <p className="text-[#9CA3AF] text-lg mb-8 mt-3">
+        Twelve lifts covering every major muscle group.
+      </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
         {libraries.map((library) => (
           <LibraryCard key={library.id} library={library} />

872cbebefe86a86b43c7034821a18495c09d7acd
Aspin Chakma <aspinchakma2019@gmail.com>
2026-09-25 23:48:19 +0600

    reponsive for workout details

--- app/components/LibraryDetailsButton.tsx
+++ app/components/LibraryDetailsButton.tsx
@@ -11,17 +11,17 @@ const LibraryDetailsButton = ({ library }: { library: Library }) => {
   const { handleAddPlans, handleAddToSaved } = context;
 
   return (
-    <div className="flex items-center gap-3 mt-4">
+    <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
       <button
         onClick={() => handleAddPlans(library)}
-        className="text-[14px] font-semibold flex items-center gap-2 px-6 py-3 rounded-xl border-2 border-[#374151] cursor-pointer hover:bg-[#CCFF00] hover:text-black transition duration-700"
+        className="text-[14px] font-semibold flex items-center gap-2 px-6 py-3 rounded-xl border-2 border-[#374151] cursor-pointer hover:bg-[#CCFF00] hover:text-black transition duration-700 w-full sm:w-fit justify-center"
       >
         <FaRegCalendarPlus />
         <span> Add to today&apos;s plan</span>
       </button>
       <button
         onClick={() => handleAddToSaved(library)}
-        className="text-[14px] font-semibold flex items-center gap-2 px-6 py-3 rounded-xl border-2 border-[#374151] cursor-pointer hover:bg-[#CCFF00] hover:text-black transition duration-700"
+        className="text-[14px] font-semibold flex items-center gap-2 px-6 py-3 rounded-xl border-2 border-[#374151] cursor-pointer hover:bg-[#CCFF00] hover:text-black transition duration-700 w-full sm:w-fit justify-center"
       >
         <FaRegBookmark />
         <span> Save for later</span>
